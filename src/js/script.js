654309ce818b3b669c9219d842258076e267bfb4
Oleg <30oleg.zv94@gmail.com>
2022-03-10 10:35:12 +0300

    modified function setinterval

--- src/js/script.js
+++ src/js/script.js
@@ -2,23 +2,19 @@ const secondArrow = document.querySelector('.clock-seconds');
 const minuteArrow = document.querySelector('.clock-minutes');
 const hourArrow = document.querySelector('.clock-hour');
 
-function setTime() {
-  let date = new Date();
+// setInterval(function setTime() {
+//   let date = new Date();
 
-  let seconds = date.getSeconds();
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
+//   let seconds = date.getSeconds();
+//   let minutes = date.getMinutes();
+//   let hours = date.getHours();
 
-  let secondsDegree = (seconds / 60) * 360 + 90;
-  secondArrow.style.transform = `rotate(${secondsDegree}deg)`;
+//   let secondsDegree = (seconds / 60) * 360 + 90;
+//   secondArrow.style.transform = `rotate(${secondsDegree}deg)`;
 
-  let minutesDegree = (minutes / 60) * 360 + 90;
-  minuteArrow.style.transform = `rotate(${minutesDegree}deg)`;
+//   let minutesDegree = (minutes / 60) * 360 + 90;
+//   minuteArrow.style.transform = `rotate(${minutesDegree}deg)`;
 
-  let hoursDegree = (hours / 12) * 360 + 90;
-  hourArrow.style.transform = `rotate(${hoursDegree}deg)`;
-
-  setTimeout(setTime, 1000);
-}
-
-setTime();
+//   let hoursDegree = (hours / 12) * 360 + 90;
+//   hourArrow.style.transform = `rotate(${hoursDegree}deg)`;
+// }, 1000);
